2d9af60e48bf0ebdff07af19655fca779d6163c2
Doni0 <doniagustian0@gmail.com>
2017-12-24 21:30:18 +0700

    Add files via upload

---
+++ src/bot2.js
@@ -0,0 +1,42 @@
+const LineConnect = require('./connect');
+const LINE = require('./main.js');
+console.info("\n\
+=========================================\n\
+BotName: LINE Alphat JS\n\
+Version: FORKED VERSION\n\
+Thanks to @Doni @OnePiece_TEAM\n\
+=========================================\n\
+\nNOTE : This bot is made by @Doni @OnePiece_TEAM and has been forked by @OnePiece !\n\
+***Copyright belongs to the author***");
+
+/*
+| This constant is for auth/login
+| 
+| Change it to your authToken / your email & password
+*/
+const auth = {
+	authToken: '',
+	certificate: '',
+	email: '',
+	password: ''
+}
+
+let client =  new LineConnect();
+//let client =  new LineConnect(auth);
+
+client.startx().then(async (res) => {
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+		//LINE.aLike() //AutoLike (CAUSE LAG)
+	}
+});
